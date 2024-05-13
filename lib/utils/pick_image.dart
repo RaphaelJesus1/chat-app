05c7429e7dff0947046ddbf26857386bb2495b7d
Raphael Menezes <raphael-mj@hotmail.com>
2024-05-12 23:20:04 -0300

    feature: pick image function

---
+++ lib/utils/pick_image.dart
@@ -0,0 +1,7 @@
+import 'package:image_picker/image_picker.dart';
+
+Future<XFile?> pickImage(ImageSource source) async {
+  final ImagePicker imagePicker = ImagePicker();
+  XFile? file = await imagePicker.pickImage(source: source);
+  return file;
+}
