b1fced381e9dcbb1f7ac90c0379ddb57fbd99ee7
Raphael Menezes <raphael-mj@hotmail.com>
2024-05-13 22:40:08 -0300

    fix: removing unecessary splash screen imports

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,7 +1,4 @@
-import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'login.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
