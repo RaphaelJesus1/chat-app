f64374f99eac4b8dd89bdd10cac909b1bd51d291
Raphael Menezes <raphael-mj@hotmail.com>
2024-05-13 22:06:03 -0300

    feature: send chat messages

--- lib/widgets/chat_input.dart
+++ lib/widgets/chat_input.dart
@@ -1,7 +1,54 @@
+import 'package:chat_app/utils/firebase.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ChatInput extends StatelessWidget {
+class ChatInput extends StatefulWidget {
   const ChatInput({super.key});
+
+  @override
+  State<ChatInput> createState() => _ChatInputState();
+}
+
+class _ChatInputState extends State<ChatInput> {
+  final _messageController = TextEditingController();
+
+  Future<void> _sendToBackend(String message) async {
+    try {
+      final user = auth.currentUser!;
+      final userData = await database.collection("users").doc(user.uid).get();
+
+      await database.collection("chat").add({
+        "text": message,
+        "createdAt": Timestamp.now(),
+        "userId": auth.currentUser!.uid,
+        "username": userData.data()!["username"],
+        "userImage": userData.data()!["profile_url"]
+      });
+    } on FirebaseException catch (error) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Failed to send message")));
+    }
+  }
+
+  void _sendMessage() async {
+    final enteredMessage = _messageController.text;
+    if (enteredMessage.trim().isEmpty) {
+      return;
+    }
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _messageController.clear();
+    });
+
+    await _sendToBackend(enteredMessage);
+  }
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +61,19 @@ class ChatInput extends StatelessWidget {
               mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                const Expanded(
+                Expanded(
                   child: TextField(
-                    decoration: InputDecoration(
-                      label: Text("Send a message..."),
+                    controller: _messageController,
+                    textCapitalization: TextCapitalization.sentences,
+                    autocorrect: true,
+                    enableSuggestions: true,
+                    decoration: const InputDecoration(
+                      labelText: "Send a message...",
                     ),
                   ),
                 ),
                 IconButton(
-                    onPressed: () {},
+                    onPressed: _sendMessage,
                     icon: Icon(
                       Icons.send,
                       color: Theme.of(context).colorScheme.primary,
