01507991a5fee834b973240b38e7a0d7c7755582
Raphael Menezes <raphael-mj@hotmail.com>
2024-05-14 23:51:27 -0300

    feature: chat message topic subscription

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -3,11 +3,27 @@ import 'package:chat_app/widgets/chat/chat_input.dart';
 import 'package:chat_app/widgets/chat/chat_messages.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
 
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
   void _logout() {
     auth.signOut();
+  }
+
+  void setupPushNotifications() async {
+    await message.requestPermission();
+    message.subscribeToTopic("chat");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
   }
 
   @override
